7129da25e5d326aa7a5d8606f0ad7467637bb82c
Worathep Seenuan <worathep.see@gmail.com>
2026-02-24 23:39:32 +0700

    Fix codes

--- bot.py
+++ bot.py
@@ -2,7 +2,7 @@ from locust import HttpUser, task, between
 
 class BackendUser(HttpUser):
     wait_time = between(0.5, 1.5)   
-    host = "http://188.166.214.193:8080"  
+    host = "http://127.0.0.1:8080"  
 
     @task
     def health_check(self):
